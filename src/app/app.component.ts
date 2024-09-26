f5f8c21a0b313496231ef00e7e1da2af49ee592e
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-09-26 13:27:37 +0200

    exercice 3.1 blinding Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ArticleComponentComponent } from './article-component/article-component.component';
@@ -5,10 +6,16 @@ import { ArticleComponentComponent } from './article-component/article-component
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ArticleComponentComponent],
+  imports: [RouterOutlet, ArticleComponentComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Anne-Sophie';
+  title: string = "Bonjour c' est Anne-Sophie";
+
+  isAdmin: boolean = false;
+
+  onClickChangeAdmin() {
+    this.isAdmin = !this.isAdmin;
+  }
 }
