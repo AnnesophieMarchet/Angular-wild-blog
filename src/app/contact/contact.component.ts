01f803f78103d8b72f22cabcabe14045fc542ff6
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-09-30 16:25:11 +0200

    exercice 07 Angular quete Les Reactive Forms

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ContactFormComponent } from '../contact-form/contact-form.component';
+import { SignupFormComponent } from '../signup-form/signup-form.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [ContactFormComponent],
+  imports: [ContactFormComponent, SignupFormComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
